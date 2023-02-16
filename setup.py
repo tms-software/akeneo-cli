8a36ee6bc50df9773672d56357010b4854eccb58
Franck <franck.coutouly@tms-software.ch>
2023-02-16 15:18:32 +0100

    Fix patch sub code

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="akeneo-cli",
-    version="1.0.7",
+    version="1.0.8",
     url="https://github.com/tms-software/akeneo-cli",
     author="Franck COUTOULY",
     author_email="franck.coutouly@tms-software.ch",
